96fd98b349282e17bc63c3eb925aa0e30ecf3760
Jace <livingfern1@live.com>
2022-08-19 12:59:36 -0400

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ArmbandCore
+Client-side mod
+
+This alters the armband slot in EFT to allow the armbands to function as armor. Without this, armbands can have the properties of armor, but will not provide the protection.
+
+### Required Steps before Building
+Run Server, Launcher, and then EFT at least once prior to referencing the dll files for this project.
+
+### Requires following assemblies for reference:
+1. Assembly-CSharp.dll
+   - Find in "{SPT_Folder}\EscapeFromTarkov_Data\Managed".
+2. Aki.Common.dll
+   - Part of the SPT modules for the launcher. Find in "{SPT_Folder}\EscapeFromTarkov_Data\Managed".
+3. BepInEx.dll
+   - Part of the BepInEx Harmony Patcher. Find in "{SPT_Folder}\BepInEx\core"
+
+Requires following files for SPT to load the module:
+1. Key.snk
+   - This is something that you must build on your own.
---
+++ project/ArmBandCore/Core.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using EFT.InventoryLogic;
+using Aki.Reflection;
+using Aki.Reflection.Patching;
+using System.Reflection;
+
+namespace Core
+{
+    [BepInPlugin("com.jbs4bmx.ArmBandCore", "ArmBandCore", "321.0.1")]
+    public class Core : BaseUnityPlugin
+    {
+        private void Main()
+        {
+            // Plugin startup logic
+            Logger.LogInfo("ArmBandCore v321.0.1 is loading...");
+            AddArmBandArmorSlot();
+            Logger.LogInfo("ArmBandCore v321.0.1 has loaded!");
+        }
+
+        // Patch
+        public void AddArmBandArmorSlot()
+        {
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
+            var field = typeof(InventoryClass).GetField("ArmorSlots", bindingFlags);
+
+            field.SetValue(null, new EquipmentSlot[]
+            {
+                EquipmentSlot.TacticalVest,
+                EquipmentSlot.ArmorVest,
+                EquipmentSlot.Headwear,
+                EquipmentSlot.FaceCover,
+                EquipmentSlot.Eyewear,
+                EquipmentSlot.ArmBand
+            });
+        }
+    }
+}
