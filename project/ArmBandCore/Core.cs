ce78c1e31ee476206e7f52dc3958b0f6604bc717
Jace <livingfern1@live.com>
2025-03-29 15:27:43 -0400

    3.11.1 Support
    3.11.1 Support

--- project/ArmBandCore/Core.cs
+++ project/ArmBandCore/Core.cs
@@ -1,38 +1,63 @@
-﻿using ABCore.Utilities;
-using BepInEx;
+﻿using BepInEx;
+using BepInEx.Configuration;
 using EFT.InventoryLogic;
+using HarmonyLib;
 using System;
 using System.Reflection;
+using static ArmBandCore.Utilities.VersionChecker;
 
-namespace ABCore
+namespace ArmBandCore
 {
     [BepInPlugin("com.jbs4bmx.ArmBandCore", "ArmBandCore", "311.1.1")]
     [BepInDependency("com.SPT.core", "3.11.1")]
     public class Core : BaseUnityPlugin
     {
         public const int TarkovVersion = 35392;
+        internal static ConfigEntry<bool> DisableBT;
 
-        private void Main()
+        private void Awake()
         {
-            if (!VersionChecker.CheckEftVersion(Logger, Info, Config))
+            var harmony = new Harmony("com.jbs4bmx.ArmBandCore");
+            const string sectionName = "ArmBandCore BT Remover";
+
+            if (!CheckEftVersion(Logger, Info, Config))
             {
                 throw new Exception("Invalid EFT Version");
             }
+
+            DisableBT = Config.Bind(
+                sectionName,
+                "Disable blunt throughput damage.",
+                true,
+                new ConfigDescription
+                (
+                    "Disable damage from blunt-force trauma?",
+                    null,
+                    new ConfigurationManagerAttributes
+                    {
+                        Order = 1,
+                    }
+                )
+            );
+
             AddArmBandArmorSlot();
+            harmony.PatchAll();
         }
 
         public void AddArmBandArmorSlot()
         {
+            
+
             var bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
             var field = typeof(Inventory).GetField("ArmorSlots", bindingFlags);
 
             field.SetValue(null, new EquipmentSlot[]
             {
-                EquipmentSlot.TacticalVest,
-                EquipmentSlot.ArmorVest,
                 EquipmentSlot.Headwear,
                 EquipmentSlot.FaceCover,
                 EquipmentSlot.Eyewear,
+                EquipmentSlot.TacticalVest,
+                EquipmentSlot.ArmorVest,
                 EquipmentSlot.ArmBand
             });
         }
---
+++ project/ArmBandCore/Utilities/BTDisablePatch.cs
@@ -0,0 +1,37 @@
+﻿using Comfort.Common;
+using EFT;
+using EFT.InventoryLogic;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace ArmBandCore.Utilities
+{
+    [HarmonyPatch(typeof(ArmorComponent), "BluntThroughput", MethodType.Getter)]
+    internal class BTDisable
+    {
+        static void Postfix(ref float __result, ArmorComponent __instance)
+        {
+            Player player = Singleton<GameWorld>.Instance?.MainPlayer;
+
+            if (player != null && player.IsYourPlayer)
+            {
+                if (!Core.DisableBT.Value)
+                {
+                    return;
+                }
+
+                List<ArmorComponent> armorComponents = new List<ArmorComponent>(20);
+                player.Inventory.GetPutOnArmorsNonAlloc(armorComponents);
+
+                foreach (ArmorComponent armor in armorComponents)
+                {
+                    if (armor.Item.TemplateId == "66087622e26587d9430a1cfb")
+                    {
+                        __result = 0f;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
