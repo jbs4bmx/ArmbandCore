97459e535a3756aed835208563220e6d20cd827e
Jace <livingfern1@live.com>
2025-03-31 18:32:08 -0400

    3.11.2 support
    3.11.2 support

--- project/ArmBandCore/Core.cs
+++ project/ArmBandCore/Core.cs
@@ -8,8 +8,8 @@ using static ArmBandCore.Utilities.VersionChecker;
 
 namespace ArmBandCore
 {
-    [BepInPlugin("com.jbs4bmx.ArmBandCore", "ArmBandCore", "311.1.1")]
-    [BepInDependency("com.SPT.core", "3.11.1")]
+    [BepInPlugin("com.jbs4bmx.ArmBandCore", "ArmBandCore", "311.2.1")]
+    [BepInDependency("com.SPT.core", "3.11.2")]
     public class Core : BaseUnityPlugin
     {
         public const int TarkovVersion = 35392;
